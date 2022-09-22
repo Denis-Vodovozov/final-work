44656964b19f6cb06f9c47ea320240851551b056
Denis Vodovozov <denniboy@yandex.ru>
2022-09-22 15:29:55 +0300

    add metod to create and show new string array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] CreateArray(int size)
+{
+    string[] array = new string[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Input {i + 1} element of array: ");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+Console.Write("Input size for array: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string[] myArray = CreateArray(a);
+ShowArray(myArray);
+Console.WriteLine();
+
